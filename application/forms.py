2e4c6266b675546274d443c9281292010efb91fd
Ashley-Edge <ashleyedge89@gmail.com>
2021-02-08 00:07:06 +0000

    Failed to add category & description options in clothing

--- application/forms.py
+++ application/forms.py
@@ -9,3 +9,15 @@ from wtforms import TextField, StringField, SubmitField, HiddenField, SelectFiel
 from wtforms.validators import DataRequired, Length, ValidationError, Email
 #from app import db
 from application.models import *
+
+##########  New user    ##########
+
+type_of_clothing =['top', 'botton', 'dress']
+
+class NewClothing(FlaskForm):
+    clothing_id = HiddenField('id')
+    name = StringField('name', validators =[Length(min=2, max=200)])
+#    category = SelectField('category', choices=[(typ, typ) for typ in type_of_clothing])
+#    description = TextField('description', validators=[Length(min=2, max=500)])
+
+
